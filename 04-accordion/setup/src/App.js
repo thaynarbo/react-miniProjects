3d0e65435354b02cd36cc45523cda8993872987e
thaynar96 <thaynarbo@gmail.com>
2021-09-29 17:07:26 -0300

    changed callback argument

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -7,8 +7,10 @@ function App() {
 			<div className='container'>
 				<h3>questions and answers about login</h3>
 				<section className='info'>
-					{data.map((um) => {
-						return <SingleQuestion key={um.id} {...um} />;
+					{data.map((question) => {
+						return (
+							<SingleQuestion key={question.id} {...question} />
+						);
 					})}
 				</section>
 			</div>
