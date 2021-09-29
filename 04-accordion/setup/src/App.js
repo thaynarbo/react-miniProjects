58e028ff995a58903f84f65b6aab2962086c2327
thaynar96 <thaynarbo@gmail.com>
2021-09-29 17:06:45 -0300

    toggle and ternary operator

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -2,7 +2,18 @@ import React, { useState } from 'react';
 import data from './data';
 import SingleQuestion from './Question';
 function App() {
-  return <h2>accordion project setup</h2>;
+	return (
+		<main>
+			<div className='container'>
+				<h3>questions and answers about login</h3>
+				<section className='info'>
+					{data.map((um) => {
+						return <SingleQuestion key={um.id} {...um} />;
+					})}
+				</section>
+			</div>
+		</main>
+	);
 }
 
 export default App;
