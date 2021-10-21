0193913c5bc06ff0081dc16ac6480cdefc8b1a6f
thaynar96 <thaynarbo@gmail.com>
2021-10-21 16:55:55 -0300

    Set up routing

--- 15-cocktails/setup/src/App.js
+++ 15-cocktails/setup/src/App.js
@@ -1,18 +1,24 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // import pages
-import Home from './pages/Home'
-import About from './pages/About'
-import SingleCocktail from './pages/SingleCocktail'
-import Error from './pages/Error'
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SingleCocktail from "./pages/SingleCocktail";
+import Error from "./pages/Error";
 // import components
-import Navbar from './components/Navbar'
+import Navbar from "./components/Navbar";
 function App() {
   return (
-    <div>
-      <h2>app component</h2>
-    </div>
-  )
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/cocktail/:id" component={SingleCocktail} />
+        <Route exact path="*" component={Error} />
+      </Switch>
+    </Router>
+  );
 }
 
-export default App
+export default App;
